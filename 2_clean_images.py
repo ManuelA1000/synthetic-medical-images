637757c3e5f4c3874852efcc89e5f20be19cfd58
Aaron Coyner <coyner@ohsu.edu>
2021-04-02 14:51:41 -0700

    Remove low-level pixel information

---
+++ 2_clean_images.py
@@ -0,0 +1,13 @@
+import os
+import cv2
+
+
+path = './out/gan/'
+
+for root, dirs, files in os.walk(path):
+    for name in files:
+        if name.endswith(('.png')):
+            img = cv2.imread(os.path.join(root, name))
+            img[img < 26] = 0
+            cv2.imwrite(os.path.join(root, name), img)
+
