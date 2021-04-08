5d9e1cf35e03ed5c1f7f98e9daea42146288fd39
Aaron Coyner <coyner@ohsu.edu>
2021-04-08 11:11:31 -0700

    Reduce path specification complexity

--- 2_clean_data.py
+++ 2_clean_data.py
@@ -3,16 +3,14 @@ import cv2
 
 
 def clean_images(path):
-	for root, dirs, files in os.walk(path):
-	    for index, name in enumerate(files):
-	        if name.endswith(('.png')):
-	            img = cv2.imread(os.path.join(root, name))
-	            img[img < 26] = 0
-	            cv2.imwrite(os.path.join(root, name), img)
-	            if index % 100 == 0:
-	            	print(f'Files completed: {index + 1}')
-	            	print(f'Current location: {root}')
+    for root, dirs, files in os.walk(path):
+        print(f'Current location: {root}')
+        for index, name in enumerate(files):
+            if name.endswith(('.png')):
+                img = cv2.imread(os.path.join(root, name))
+                img[img < 26] = 0
+                cv2.imwrite(os.path.join(root, name), img)
+                    
 
 
-clean_images(path = './out/gan/')
-clean_images(path = './out/cnn/')
+clean_images(path = './out/')
