f59159bfef850d23541b82290afb4d973d5e7024
Aaron Coyner <coyner@ohsu.edu>
2021-04-20 11:27:27 -0700

    Update original segmentations to BMP

--- 2_clean_data.py
+++ 2_clean_data.py
@@ -3,14 +3,16 @@ import cv2
 
 
 def clean_images(path):
-    for root, dirs, files in os.walk(path):
-        print(f'Current location: {root}')
-        for index, name in enumerate(files):
-            if name.endswith(('.png')):
-                img = cv2.imread(os.path.join(root, name))
-                img[img < 26] = 0
-                cv2.imwrite(os.path.join(root, name), img)
-                    
+	for root, dirs, files in os.walk(path):
+		print(f'Current location: {root}')
+		for index, name in enumerate(files):
+			if name.endswith(('.bmp')):
+				png_name = name.split('.')[0] + '.png'
+				img = cv2.imread(os.path.join(root, name))
+				img[img < 26] = 0
+				cv2.imwrite(os.path.join(root, png_name), img)
+				os.remove(os.path.join(root, name))
+					
 
 
 clean_images(path = './out/')
