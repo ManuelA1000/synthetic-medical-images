fcd02d8f71d930afd8106d5f0fcf7b2a2ee310ad
Aaron Coyner <coyner@ohsu.edu>
2021-04-28 14:17:55 -0700

    Rename test scripts

---
+++ cycle_gan/test_forward.sh
@@ -0,0 +1,18 @@
+python test.py --model cycle_gan --name normal2plus --dataroot ../out/cnn/train/real/1
+
+python test.py --model cycle_gan --name normal2preplus --dataroot ../out/cnn/train/real/1
+
+python test.py --model cycle_gan --name preplus2plus --dataroot ../out/cnn/train/real/2
+
+
+cd results/normal2plus/test_latest/images/
+rm -v !(*fake_B*)
+cd ../../../..
+
+cd results/normal2preplus/test_latest/images/
+rm -v !(*fake_B*)
+cd ../../../..
+
+cd results/preplus2plus/test_latest/images/
+rm -v !(*fake_B*)
+cd ../../../..
