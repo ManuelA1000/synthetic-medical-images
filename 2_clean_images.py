ade3735d9b6f7d28d570962e33543c7173b47eaa
Aaron Coyner <coyner@ohsu.edu>
2021-10-19 08:30:39 -0700

    Revert to jpgs

--- 2_clean_images.py
+++ 2_clean_images.py
@@ -8,7 +8,8 @@ for root, dirs, files in os.walk('./out'):
        if name.endswith('.bmp'):
            input_path = os.path.join(root, name)
            img = cv2.imread(input_path)
-           img[img < 26] = 0
+           img = cv2.resize(img, (256,256))
+           img[img < 51] = 0
            save_path = os.path.splitext(input_path)[0] + '.png'
            cv2.imwrite(save_path, img)
            os.remove(input_path)
