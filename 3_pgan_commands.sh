c515ed6360e66b7b367055cc5e933b0b6879ba3b
Aaron Coyner <coyner@ohsu.edu>
2021-10-21 18:15:49 -0700

    Change synthetic dataset size and add FID commands

--- 3_pgan_commands.sh
+++ 3_pgan_commands.sh
@@ -3,25 +3,17 @@ python ./pytorch_GAN_zoo/train.py PGAN --no_vis -c ./pytorch_GAN_zoo/configs/con
 python ./pytorch_GAN_zoo/train.py PGAN --no_vis -c ./pytorch_GAN_zoo/configs/config_pre-plus.json -n preplus
 python ./pytorch_GAN_zoo/train.py PGAN --no_vis -c ./pytorch_GAN_zoo/configs/config_plus.json -n plus
 
-
-## inception score
-python ./pytorch_GAN_zoo/eval.py inception -m PGAN -d ./out/output_networks --no_vis -n normal
-python ./pytorch_GAN_zoo/eval.py inception -m PGAN -d ./out/output_networks --no_vis -n preplus
-python ./pytorch_GAN_zoo/eval.py inception -m PGAN -d ./out/output_networks --no_vis -n plus
-
-
 ## synthesize    NOTE: using --no_vis causes error
-python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --save_dataset ./out/train/synth_even/1 --size_dataset 7752 -d ./out/output_networks -n normal
-python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --save_dataset ./out/train/synth_even/2 --size_dataset 1305 -d ./out/output_networks -n preplus
-python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --save_dataset ./out/train/synth_even/3 --size_dataset 270 -d ./out/output_networks -n plus
-
+python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --save_dataset ./data/train/synth_even/1 --size_dataset 14540 -d ./out/output_networks -n normal
+python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --save_dataset ./data/train/synth_even/2 --size_dataset 2320 -d ./out/output_networks -n preplus
+python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --save_dataset ./data/train/synth_even/3 --size_dataset 525 -d ./out/output_networks -n plus
 
 # frechet inception distance
-python -m pytorch_fid --dims 64 train/real/1 test/1
-python -m pytorch_fid --dims 64 train/real/1 train/synth/1
+python pytorch-fid/src/pytorch_fid/fid_score.py --dims 64 ./out/train/real/1 ./out/val_test_combined/1
+python pytorch-fid/src/pytorch_fid/fid_score.py --dims 64 ./out/train/real/1 ./out/train/synth/1
 
-python -m pytorch_fid --dims 64 train/real/2 test/2
-python -m pytorch_fid --dims 64 train/real/2 train/synth/2
+python pytorch-fid/src/pytorch_fid/fid_score.py --dims 64 ./out/train/real/2 ./out/val_test_combined/2
+python pytorch-fid/src/pytorch_fid/fid_score.py --dims 64 ./out/train/real/2 ./out/train/synth/2
 
-python -m pytorch_fid --dims 64 train/real/3 test/3
-python -m pytorch_fid --dims 64 train/real/3 train/synth/3
+python pytorch-fid/src/pytorch_fid/fid_score.py --dims 64 ./out/train/real/3 ./out/val_test_combined/3
+python pytorch-fid/src/pytorch_fid/fid_score.py --dims 64 ./out/train/real/3 ./out/train/synth/3
