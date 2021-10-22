33b35442a297315d8b870fddcaee83241840d867
Aaron Coyner <coyner@ohsu.edu>
2021-10-21 18:15:25 -0700

    Update 2_clean_images.py

--- 2_clean_images.py
+++ 2_clean_images.py
@@ -3,15 +3,21 @@ import os
 import cv2
 from tqdm import tqdm
 
-for root, dirs, files in os.walk('./out'):
-   for name in tqdm(files):
-       if name.endswith('.bmp'):
-           input_path = os.path.join(root, name)
-           img = cv2.imread(input_path)
-           img = cv2.resize(img, (256,256))
-           img[img < 51] = 0
-           save_path = os.path.splitext(input_path)[0] + '.png'
-           cv2.imwrite(save_path, img)
-           os.remove(input_path)
-       else:
-           pass
+
+
+IMAGE_EXTENSIONS = {'.bmp', '.jpg', '.jpeg','.png', '.tif', '.tiff'}
+
+for root, dirs, files in os.walk('./data'):
+    for name in tqdm(files):
+        basename, ext = os.path.splitext(name)
+        if ext.casefold() in IMAGE_EXTENSIONS:
+            input_path = os.path.join(root, name)
+            img = cv2.imread(input_path)
+            img = cv2.resize(img, (256,256))
+            img[img < 51] = 0
+            save_path = os.path.join(root, basename + '.png')
+            cv2.imwrite(save_path, img)
+            if ext.casefold() != '.png':
+                os.remove(input_path)
+        else:
+            pass
