c993b6cd1bc60ebdddbf8ab716917a38552bac52
Aaron Coyner <coyner@ohsu.edu>
2021-11-17 18:51:45 -0800

    Make datasets same size as real image datasets

--- 3_pgan_commands.sh
+++ 3_pgan_commands.sh
@@ -4,10 +4,10 @@ python ./pytorch_GAN_zoo/train.py PGAN --no_vis -n preplus -c ./pytorch_GAN_zoo/
 python ./pytorch_GAN_zoo/train.py PGAN --no_vis -n plus -c ./pytorch_GAN_zoo/configs/config_plus.json
 
 ## synthesize
-python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/train/synthetic/1 --size_dataset 2000 -n normal
-python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/train/synthetic/2 --size_dataset 2000 -n preplus
-python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/train/synthetic/3 --size_dataset 2000 -n plus
+python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/train/synthetic/1 --size_dataset 2908 -n normal
+python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/train/synthetic/2 --size_dataset 464 -n preplus
+python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/train/synthetic/3 --size_dataset 105 -n plus
 
-python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/val/synthetic/1 --size_dataset 500 -n normal
-python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/val/synthetic/2 --size_dataset 500 -n preplus
-python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/val/synthetic/3 --size_dataset 500 -n plus
+python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/val/synthetic/1 --size_dataset 903 -n normal
+python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/val/synthetic/2 --size_dataset 222 -n preplus
+python ./pytorch_GAN_zoo/eval.py visualization -m PGAN --np_vis -d ./out/output_networks --save_dataset ./data/val/synthetic/3 --size_dataset 42 -n plus
